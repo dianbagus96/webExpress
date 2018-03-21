dcaab3ba085ecfe3589f37d28bae26e1457889af
herdian bagus <herdian.bagus@edi-indonesia.co.id>
2018-03-21 23:26:41 +0700

    add nodemon module

--- server.js
+++ server.js
@@ -2,10 +2,14 @@
 var express = require("express")
     ,app = express()
     ,port = process.env.port || 3000
+    ,bodyParser = require('body-parser')
     ,routes = require("./web/routes/webRoutes");
+
+    app.use(bodyParser.urlencoded({extended : true}));
+    app.use(bodyParser.json);
 
     routes(app);
 
-app.listen(port, ()=>{
-    console.log("running on port " + port)
-})
+    app.listen(port, ()=>{
+        console.log("running on port " + port)
+    })
